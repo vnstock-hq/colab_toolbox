9a488497339a19b65ecbd62651d0e087b12b6e15
Thinh Vu <39723360+thinh-vu@users.noreply.github.com>
2024-07-17 05:31:07 +0000

    Reorder installation script

--- colab_toolbox/script/setup_ocrmypdf.sh
+++ colab_toolbox/script/setup_ocrmypdf.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
-sudo apt-get -y install ocrmypdf python3-pip
-pip install --user --upgrade ocrmypdf
-sudo apt install tesseract-ocr
 apt-get install -y poppler-utils
-pip install pytesseract
+sudo apt install tesseract-ocr
 sudo apt-get install tesseract-ocr-vie # Cài đặt gói ngôn ngữ tiếng Việt
+sudo apt-get -y install ocrmypdf python3-pip
+pip install pytesseract
+pip install --user --upgrade ocrmypdf
