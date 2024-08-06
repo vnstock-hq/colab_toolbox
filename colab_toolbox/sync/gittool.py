9eb675dad5992612aae327620113780ca8435291
Thinh Vu <39723360+thinh-vu@users.noreply.github.com>
2024-08-06 06:19:44 +0000

    bug fixed on git command

--- colab_toolbox/sync/gittool.py
+++ colab_toolbox/sync/gittool.py
@@ -121,13 +121,16 @@ class GitTool:
             error_message = e.stderr.decode('utf-8') if e.stderr else "An error occurred during push."
             print(f"Error during push: {error_message}")
     
-    def pull(self):
+    def pull(self, branch: Union[str, None]=None):
         """
         Pull changes from the Git repository.
         """
         try:
             os.chdir(self.repo_dir)
-            result = subprocess.run(f"git pull https://{self.access_key}@github.com/{self.user_name}/{self.repo_name}.git {branch}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+            if branch:
+                result = subprocess.run(f"git pull https://{self.access_key}@github.com/{self.user_name}/{self.repo_name}.git {branch}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+            else:
+                result = subprocess.run(f"git pull https://{self.access_key}@github.com/{self.user_name}/{self.repo_name}.git", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
             print(result.stdout.decode('utf-8'))
         except subprocess.CalledProcessError as e:
             error_message = e.stderr.decode('utf-8') if e.stderr else "An error occurred during pull."
