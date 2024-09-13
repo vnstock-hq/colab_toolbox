1eceddcd736718475d68dce8e536b5f030726dae
Thinh Vu <39723360+thinh-vu@users.noreply.github.com>
2024-09-13 16:17:31 +0700

    Update setup_selenium.sh

--- colab_toolbox/script/setup_selenium.sh
+++ colab_toolbox/script/setup_selenium.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
-# Update the package list
-sudo apt -y update
-
-# Install necessary packages
-sudo apt install -y wget curl unzip
-
 # Download and install the libu2f-udev package
 wget http://archive.ubuntu.com/ubuntu/pool/main/libu/libu2f-host/libu2f-udev_1.1.4-1_all.deb
 sudo dpkg -i libu2f-udev_1.1.4-1_all.deb
@@ -14,8 +6,8 @@ sudo dpkg -i libu2f-udev_1.1.4-1_all.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb || sudo apt -f install -y
 
-# Get the latest version of ChromeDriver
-CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+# Get the version of ChromeDriver installed
+CHROME_DRIVER_VERSION=$(dpkg -s google-chrome-stable | grep '^Version:' | grep -oP '\d+\.\d+\.\d+\.\d+')
 wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P /tmp/
 
 # Unzip and set permissions for ChromeDriver
